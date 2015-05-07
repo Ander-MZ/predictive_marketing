c6ce8ba818811247a70523d1c0c0af9c4a9e681c
Ander Murillo <ander_mz@hotmail.com>
2015-05-06 19:26:51 -0500

    Includes a data profiling script

--- mainTester.sh
+++ mainTester.sh
@@ -8,4 +8,6 @@ bash tester.sh ~/Storage/data/2013-04-01_2013-08-01_data_sorted.csv 04-08
 
 echo "04-09"
 
+python dataProfiler.py -i ~/Storage/data/2013-04-01_2013-09-01_data_sorted.csv 
+
 bash tester.sh ~/Storage/data/2013-04-01_2013-09-01_data_sorted.csv 04-09
