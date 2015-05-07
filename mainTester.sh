458f96d99fa7a05e4c8dbdc0eb7de7af32654304
Ander Murillo <ander_mz@hotmail.com>
2015-05-06 19:01:00 -0500

    Updated for new datasets

--- mainTester.sh
+++ mainTester.sh
@@ -2,22 +2,10 @@
 
 echo "Evaluating models"
 
-echo "04-06"
+echo "04-08"
 
-bash tester.sh ~/Storage/data/04-06_sorted_lite.csv 04-06
+bash tester.sh ~/Storage/data/2013-04-01_2013-08-01_data_sorted.csv 04-08
 
-echo "05-07"
+echo "04-09"
 
-bash tester.sh ~/Storage/data/05-07_sorted_lite.csv 05-07
-
-echo "06-08"
-
-bash tester.sh ~/Storage/data/06-08_sorted_lite.csv 06-08
-
-echo "07-09"
-
-bash tester.sh ~/Storage/data/07-09_sorted_lite.csv 07-09
-
-echo "06-09"
-
-bash tester.sh ~/Storage/data/06-09_sorted_lite.csv 06-09
+bash tester.sh ~/Storage/data/2013-04-01_2013-09-01_data_sorted.csv 04-09
